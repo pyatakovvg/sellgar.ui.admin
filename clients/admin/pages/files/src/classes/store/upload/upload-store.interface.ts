2ef4e11bfa481224ed60a21b18df21bc9f059837
Viktor Pyatakov <viktor.pyatakov@x5.ru>
2025-01-11 03:10:57 +0300

    upload images

--- clients/admin/pages/files/src/classes/store/upload/upload-store.interface.ts
+++ clients/admin/pages/files/src/classes/store/upload/upload-store.interface.ts
@@ -4,5 +4,5 @@ export abstract class UploadStoreInterface {
   abstract inProcess: boolean;
 
   abstract setProcess(state: boolean): void;
-  abstract upload(data: any, folderUuid?: string): Promise<FileEntity>;
+  abstract upload(data: any, folderUuid?: string): Promise<FileEntity[]>;
 }
