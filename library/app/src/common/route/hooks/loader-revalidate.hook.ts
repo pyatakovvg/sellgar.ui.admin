2d2c3accffe7ed5a740f5dd29db5f7840704675b
Victor Pyatakov <viktor.pyatakov@tiyn.io>
2026-02-07 09:53:54 +0300

    spiner only first loader

--- library/app/src/common/route/hooks/loader-revalidate.hook.ts
+++ library/app/src/common/route/hooks/loader-revalidate.hook.ts
@@ -1,7 +1,11 @@
-import { useRevalidator } from 'react-router-dom';
+import { useRevalidator, useNavigation } from 'react-router-dom';
 
 export const useLoaderRevalidate = () => {
   const { revalidate, state } = useRevalidator();
+  const navigation = useNavigation();
+  const inProcess = Boolean(navigation.location);
 
-  return { revalidate, isLoading: state === 'loading' };
+  console.log(123, 1, state);
+
+  return { revalidate, inProcess };
 };
